26b12d211b34b6fd238dd5d6a0dacbaac77a8c86
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-08-21 11:52:49 +1000

    merge clouddatabase branch

--- project/src/model/CoreSystem.java
+++ project/src/model/CoreSystem.java
@@ -1,17 +1,12 @@
 package model;
 
 import java.util.List;
-
-<<<<<<< HEAD:project/src/model/CoreTest.java
 import database.CloudDatabase;
 
 /**
  * A mock core for faster testing and until we get a database running.
  */
-public class CoreTest implements CoreAPI
-=======
 public class CoreSystem implements CoreAPI 
->>>>>>> ccf2bcb0d772c4ce527408b97630f724a63a071f:project/src/model/CoreSystem.java
 {
 	private UserDetails curUserSession = null;
 	private PlayerStats curPlayerStats = null;
@@ -20,11 +15,7 @@ public class CoreSystem implements CoreAPI
 	@Override
 	public boolean registerNewUser(UserDetails details) 
 	{
-<<<<<<< HEAD:project/src/model/CoreTest.java
     	CloudDatabase cd = new CloudDatabase("register", details);
-=======
-		// TODO Auto-generated method stub
->>>>>>> ccf2bcb0d772c4ce527408b97630f724a63a071f:project/src/model/CoreSystem.java
 		return true;
 	}
 
