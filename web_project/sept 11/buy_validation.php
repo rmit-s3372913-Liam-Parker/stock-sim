681f7504bef75b822ba66c023f264a348a7e040e
rmit-s3492681-Jan-Dungo <Kenn@Kenns-MacBook-Pro.local>
2017-09-21 11:14:23 +1000

    Buy function with new API
    fixed buy function to insert to player, transaction & stock tables.

--- web_project/sept 11/buy_validation.php
+++ web_project/sept 11/buy_validation.php
@@ -8,11 +8,23 @@ if(isset($_POST['buy-submit']))
 	$total = mysqli_escape_string($conn, $_POST['buy-total']);
 	$code = mysqli_escape_string($conn, $_POST['code']);
 	$share = mysqli_escape_string($conn, $_POST['share']);
+	$date = date('Y-m-d H:i:s');
 	
 	$sql = "select winning FROM player WHERE username = '" . $_SESSION['username'] . "'";
 	$result = mysqli_query($conn, $sql);
 	$row = mysqli_fetch_assoc($result);
 	$currentMoney = $row['winning'];
+
+	// check stock table
+	$sql = "select stockID FROM stock WHERE username = '" . $_SESSION['username'] . "'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	$currentStockID = $row['stockID'];
+	
+	$sql = "select stockQuantity FROM stock WHERE username = '" . $_SESSION['username'] . "'";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	$currentStockQuantity = $row['stockQuantity'];
 	
 	
 	// check if current money is 0
@@ -21,19 +33,39 @@ if(isset($_POST['buy-submit']))
 		// check if purchase amount is greater than money on hand
 		if($currentMoney >= $total)
 		{
-			// set database winning
-			$result = $currentMoney - $total;
-			$sql = "update player set winning='".$result."' WHERE username = '" . $_SESSION['username'] . "'";
-
-			// set database for transaction table
-			$uname = $_SESSION['username'];
-			
-			$sql = "INSERT INTO transaction(username, stockID, transactionType, StockQuantity, price, winningAfterTransaction) VALUES('$uname', '$code', 'Buy', '$share', '9', '$currentMoney')";
-			
-			if($conn->query($sql) == TRUE)
+			// check if stock exists
+			if($currentStockID == $code)
 			{
-				header('location: dashboard.php');
+				$newQuantity = $currentStockQuantity + $share;
+				$sql = "update stock set stockQuantity='".$newQuantity."' WHERE username = '" . $_SESSION['username'] . "';";
+
 			}
+			else
+			{
+				$uname = $_SESSION['username'];
+				$sql = "INSERT INTO stock(stockID, username, stockQuantity) VALUES('$code', '$uname', '$share');";
+			}
+				
+				// set database winning
+				$result = $currentMoney - $total;
+				$sql .= "update player set winning='".$result."' WHERE username = '" . $_SESSION['username'] . "';";
+
+				// set database table for buySellDetail
+				//$sql .= "INSERT INTO buySellDetail(stockId, quantity, price) VALUES('$code', '$share', '$total');";
+
+				// set database for transaction table
+				$uname = $_SESSION['username'];
+				$sql .= "INSERT INTO transaction(username, transactionType, postWinning, timeOfTransaction) VALUES('$uname', 'Buy', '$result', '$date')";
+				
+				if (mysqli_multi_query($conn, $sql))
+				{
+					echo 'Query executed';
+					header('location: dashboard.php');
+				}
+			// if($conn->query($sql) == TRUE)
+			// {
+			// 	header('location: dashboard.php');
+			// }
 			
 		}
 		else
@@ -49,7 +81,8 @@ if(isset($_POST['buy-submit']))
 
 	}
 
-	
+	mysqli_close($conn);
+
 
 
 }
--- web_project/sept 11/winning.php
+++ web_project/sept 11/winning.php
@@ -4,7 +4,7 @@ include 'connection.php';
 $sql = "select winning FROM player WHERE username = '" . $_SESSION['username'] . "'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
-echo $row['winning'];
+echo "$".number_format($row['winning'], 2);
 ?>
 
 
