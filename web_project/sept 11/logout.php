0406c5ba038bb62fc04f7c17e8a6e3534fd59cb0
rmit-s3524307-Quang-Anh-Nguyen <s3524307@student.rmit.edu.au>
2017-10-10 23:20:16 +1100

    clean up web project folder

--- web_project/sept 11/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// stop user session after logout
-session_start();
-unset($_SESSION['username']);
-session_destroy();
-header("Location: login.php");
-
-
-?>
--- web_project/sept 6/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (session_status !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-
-?>
